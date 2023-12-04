c4162030863be22bf1cf0c7798ee1369e267d696
Kyo628 <51012973+Kyo628@users.noreply.github.com>
2023-12-04 23:33:14 +0900

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,11 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
+./scripts/feeds update helloworld
+./scripts/feeds install -a -f -p helloworld
+sed -i "/helloworld/d" "feeds.conf.default"
+./scripts/feeds clean
+./scripts/feeds update -a
+./scripts/feeds install -a
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
