d6422bfa485873c42fd383de525ff49cb996b5c7
Kyo628 <51012973+Kyo628@users.noreply.github.com>
2023-12-04 23:59:19 +0900

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,11 +9,14 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-./scripts/feeds update helloworld
-./scripts/feeds install -a -f -p helloworld
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-./scripts/feeds clean
-./scripts/feeds update -a
-./scripts/feeds install -a
+#./scripts/feeds update helloworld
+#./scripts/feeds install -a -f -p helloworld
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#./scripts/feeds clean
+#./scripts/feeds update -a
+#./scripts/feeds install -a
+./scripts/feeds update packages
+rm -rf feeds/packages/lang/golang
+svn co https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang feeds/packages/lang/golang
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
